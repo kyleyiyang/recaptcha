15c98dcb9d491ef118b15c93e93d06331c9620a7
kyleyiyang <kyleyiyang@gmail.com>
2020-05-17 14:07:55 -0400

    Create contact_form_7.php

---
+++ contact_form_7.php
@@ -0,0 +1,11 @@
+<label>Who are you? who, who...who, who?</label>
+[text* your-name class:form-control]
+<br />
+<label>Email Address</label>
+[email* your-email class:form-control]
+<br />
+<label>Please tell us a nice story :)</label>
+[textarea your-message class:form-control]
+<br />
+[recaptcha]
+[submit class:btn "Submit"]
